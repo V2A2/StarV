ae2bf6fe56ba023db82fc37f51118e0fa33337be
bryaz7 <bryanduong93@gmail.com>
2024-07-11 14:22:09 -0500

    getMinimizedConstraints() minKowskiSum()

--- bryan/probstar.py
+++ bryan/probstar.py
@@ -5,6 +5,8 @@ Bryan Duong 7/4/2024
 """
 
 import numpy as np
+import polytope as pc
+from scipy.linalg import block_diag
 from StarV.util.minimax_tilting_sampler import TruncatedMVN
 
 
@@ -137,3 +139,50 @@ class ProbStar(object):
         ub = c + v1 @ self.pred_lb + v2 @ self.pred_ub
 
         return lb, ub
+
+    def estimateProbability(self):
+        pass
+
+    def minKowskiSum(self, Y):
+        """Calculate the Minkowski sum of two ProbStar objects."""
+
+        if not isinstance(Y, ProbStar):
+            raise TypeError("Y must be a ProbStar object")
+        if self.dim != Y.dim:
+            raise ValueError("The dimensions of the two ProbStar objects must match")
+
+        V_self = self.V.copy()
+        V_Y = Y.V.copy()
+        V_self[:, 0] += V_Y[:, 0]
+        V_Y = np.delete(V_Y, 0, 1)
+        V_new = np.hstack((V_self, V_Y))
+
+        pred_lb_new = np.concatenate((self.pred_lb, Y.pred_lb))
+        pred_ub_new = np.concatenate((self.pred_ub, Y.pred_ub))
+        mu_new = np.concatenate((self.mu, Y.mu))
+        Sig_new = block_diag(self.Sig, Y.Sig)
+        C_new = block_diag(self.C, Y.C)
+        d_new = np.concatenate((self.d, Y.d))
+
+        if len(d_new) == 0:
+            C_new, d_new = [], []
+
+        return ProbStar(V_new, C_new, d_new, mu_new, Sig_new, pred_lb_new, pred_ub_new)
+
+    def getMinimizedConstraints(self):
+        """Minimize constraints of a ProbStar object."""
+
+        if len(self.C) == 0:
+            return self.C, self.d
+
+        C_extra = np.vstack((np.eye(self.nVars), -np.eye(self.nVars)))
+        d_extra = np.concatenate([self.pred_ub, -self.pred_lb])
+
+        C_combined = np.vstack((self.C, C_extra))
+        d_combined = np.concatenate([self.d, d_extra])
+
+        P = pc.Polytope(C_combined, d_combined)
+
+        P_reduced = pc.reduce(P)
+
+        return P_reduced.A, P_reduced.b
