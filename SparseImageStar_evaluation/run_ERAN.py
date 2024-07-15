56abdc8214cc607eb7371f4636b10adf42a18906
Sung Woo Choi <csw94056@gmail.com>
2024-07-14 22:35:25 -0500

    .

--- SparseImageStar_evaluation/run_ERAN.py
+++ SparseImageStar_evaluation/run_ERAN.py
@@ -205,14 +205,14 @@ def verify_eran_network(net_type='Small', data_type='MNIST', trained_type='DiffA
     print('=====================================================')
 
 if __name__ == "__main__":
-    # verify_eran_network(net_type='Small', data_type='MNIST', trained_type='DiffAI', dtype='float64')
+    verify_eran_network(net_type='Small', data_type='MNIST', trained_type='DiffAI', dtype='float64')
     # verify_eran_network(net_type='Small', data_type='MNIST', trained_type='PGDK', dtype='float64') optimization error at eps=0.002 convSmallRELU__PGDK MNIST
     verify_eran_network(net_type='Small', data_type='MNIST', trained_type='Point', dtype='float64')
 
     verify_eran_network(net_type='Big', data_type='MNIST', trained_type='DiffAI', dtype='float64')
 
     verify_eran_network(net_type='Small', data_type='CIFAR10', trained_type='DiffAI', dtype='float64')
-    verify_eran_network(net_type='Small', data_type='CIFAR10', trained_type='PGDK', dtype='float64')
+    # verify_eran_network(net_type='Small', data_type='CIFAR10', trained_type='PGDK', dtype='float64')
     verify_eran_network(net_type='Small', data_type='CIFAR10', trained_type='Point', dtype='float64')
 
     verify_eran_network(net_type='Big', data_type='CIFAR10', trained_type='DiffAI', dtype='float64')
