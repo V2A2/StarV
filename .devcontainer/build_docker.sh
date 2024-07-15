091f138895324912597ec828343a4e81e3041530
Sung Woo Choi <csw94056@gmail.com>
2024-07-14 23:29:58 -0500

    docker gurobi

--- .devcontainer/build_docker.sh
+++ .devcontainer/build_docker.sh
@@ -1,1 +1,2 @@
 docker build . -t starv -f .devcontainer/Dockerfile
+docker-compose up --build
--- .devcontainer/launch_docker.sh
+++ .devcontainer/launch_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -v ".:/work" -it starv bash --volume=$PWD/models:/models /models/poolsearch.py
+docker run --rm -v ".:/work" -it starv bash
