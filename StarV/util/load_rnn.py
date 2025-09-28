6a37c922dde387ea34c42ca8752f01bdac7d87bd
Qing-kk <qing001124@gmail.com>
2025-09-28 18:52:51 -0400

    upload_load_rnn.py

---
+++ StarV/util/load_rnn.py
@@ -0,0 +1,43 @@
+from scipy.io import loadmat
+import os
+import mat73
+import numpy as np
+from StarV.set.star import Star
+
+
+
+def load_simple_rnn(dtype=float):
+    """Load RNN model"""
+
+    cur_path = os.path.dirname(os.path.abspath(__file__))
+    mat_contents = loadmat( cur_path + "/data/matlab/HSCC2023/small_RNN/simple_rnn.mat")
+    Whx = np.asarray(mat_contents["kernel"], dtype)              # (H x I)
+    Whh = np.asarray(mat_contents["recurrent_kernel"], dtype)    # (H x H)
+    bh = np.asarray(mat_contents["bias"], dtype).reshape(-1) # (H,)
+
+    H,I = Whx.shape
+    Woh = np.eye(2, H)                    
+    bo  = np.zeros(2,)
+
+    """Load input data points"""
+    data_contents = loadmat( cur_path + '/data/matlab/HSCC2023/small_RNN/points.mat')
+    data_points = np.asarray(data_contents["pickle_data"], dtype)     
+
+    W_contents = loadmat( cur_path + "/data/matlab/HSCC2023/small_RNN/dense.mat")
+    W_ff = np.asarray(W_contents["W"], dtype=object)
+    b_ff = np.asarray(W_contents["b"], dtype=object)
+
+    return  Whx,Whh,bh,Woh,bo,data_points, W_ff,b_ff
+
+def get_reachable_set(input_points, eps):
+
+    x = input_points
+    n = x.shape[1]
+    X = []
+    for i in range (0,n):
+        S = Star(x[:,i] - eps, x[:, i] + eps)
+        S.C = np.zeros([1,S.nVars])  
+        S.d = np.zeros([1])
+        X.append(S)
+   
+    return X
