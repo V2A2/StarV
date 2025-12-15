c5d25348acecb0c70fac4e44408a20d4ccf860a6
Qing-kk <qing001124@gmail.com>
2025-12-15 16:59:41 -0500

    update RNN_training

--- StarV/RNN_training.py
+++ StarV/RNN_training.py
@@ -6,11 +6,14 @@ from scipy.io import loadmat
 import os
 import pandas as pd
 import numpy as np
+import torch
+import torch.nn as nn
 import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
 
 
 
-class RNN_training(object):
+class RNN_dataset(object):
 
     def __init__(self):
         pass
@@ -60,8 +63,7 @@ class RNN_training(object):
         
         # df_merged_data = df_merged_data.drop("total_cycles", axis=1) 
 
-        print("\n Merged data with RUL:")
-        print(df_merged_data)
+        print("\n Merged data with RUL:",df_merged_data)
 
         return df_merged_data
 
@@ -94,9 +96,8 @@ class RNN_training(object):
 
         for unit, group in grouped_engine_data:
 
-            # print(f"--- Group: {unit} ---")
+            # print(f"--- Group: {unit} ---\n")
             # print(group) # print all rows in that group
-            # print("\n")
 
             features = group[num_vars].values # all features in each engine, each feature is the op + sensor data
 
@@ -115,16 +116,89 @@ class RNN_training(object):
                 target_rul = rul[end-1]
 
                 # print("each_window:",each_window)   
-                print("target_rul_for_cycle{} in engine{}:{}".format(start+window_size,unit,target_rul))    
+                # print("target_rul_for_cycle{} in engine{}:{}".format(start+window_size,unit,target_rul))    
                 input_seqs.append(each_window)
                 rul_value.append(target_rul)
-            print("number of input seqs for engine {}:{}".format(unit,len(input_seqs)))
+            # print("number of input seqs for engine {}:{}".format(unit,len(input_seqs)))
 
             X.extend(input_seqs)
             y.extend(rul)
         print("number of input seqs for all engines:{}".format(len(X)))
 
         return np.array(X), np.array(y)
+    
+
+class CmapssDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = torch.tensor(X, dtype=torch.float32)
+        self.y = torch.tensor(y, dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+
+class RNN_model(nn.Module):
+    def __init__(self, input_size, hidden_size, fc_sizes,dropout_prob):
+        super().__init__()
+
+        # Vanilla RNN with ReLu
+        self.rnn = nn.RNN(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=1,
+            nonlinearity='relu',
+            batch_first=True,
+            dropout=0
+        )
+
+        # apply dropout to the last hidden layer
+        self.last_hidden_layer_dropout = nn.Dropout(dropout_prob)
+
+        # Fully connected layers with ReLu
+        fc_layers = []
+        prev_layer = hidden_size
+        for s in fc_sizes:
+            fc_layers.append(nn.Linear(prev_layer, s))
+            fc_layers.append(nn.ReLU())   
+            fc_layers.append(nn.Dropout(0.2))  # FC dropout layer
+            prev_layer = s
+        
+        output_layer = nn.Linear(prev_layer, 1)  # output RUL
+
+        fc_layers.append(output_layer)
+        self.fc = nn.Sequential(*fc_layers)
+    
+    def init_weights(self):
+        # RNN initialization
+        for name, param in self.rnn.named_parameters():
+            # print(f"RNN_Name: {name} ---- Shape: {param.shape}")
+            if "weight_ih" in name:
+                nn.init.xavier_uniform_(param) # weight init from xavier uniform(-a,a)
+                # print(f"weight_ih_init:{param}")
+            elif "weight_hh" in name:
+                nn.init.orthogonal_(param)
+            elif "bias" in name:
+                nn.init.zeros_(param)
+
+        # FC layers initialization
+        for i, layer in enumerate(self.fc):
+            for name, param in layer.named_parameters():
+                # print(f"FC_layer {i}: {layer} ---- Shape: {param.shape}")
+                # if isinstance(param, nn.Linear):
+                nn.init.xavier_uniform_(layer.weight)
+                # print(f"FC_layer {i} weight initialization:{layer.weight}")
+                nn.init.zeros_(layer.bias)
+
+    def forward(self, x):
+        rnn_out, h = self.rnn(x)
+        last_hidden_state = h[-1]              
+        h_dropped = self.last_hidden_layer_dropout(last_hidden_state)
+        output = self.fc(h_dropped)
+        output = output.squeeze(-1)        
+        return output
 
     def train():
         pass
@@ -150,12 +224,32 @@ def plot_egine_cycles(df_train,index_names):
         plt.tight_layout()
         plt.show()
 
+def set_seed(seed=42):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
 
 if __name__ == "__main__":
 
-    RNN = RNN_training()
-    df_train,_,_=RNN.load_data()
-    merged_data = RNN.Merged_with_RUL(df_train)
+    set_seed(25) 
+    data = RNN_dataset()
+    df_train,_,_=data.load_data()
+    merged_data = data.Merged_with_RUL(df_train)
     # plot_egine_cycles(df_train,index_names)
     win_size =20
-    X,y= RNN.create_input_sequnces(merged_data,win_size)
+    input_seqs,targt= data.create_input_sequnces(merged_data,win_size)
+    print("input_seqs_shape:",input_seqs.shape)
+
+    X = torch.tensor(input_seqs, dtype=torch.float32)
+    print("X_tshape:",X.shape)
+    y = torch.tensor(targt, dtype=torch.float32)
+
+
+    model= RNN_model(input_size=24, hidden_size=32, fc_sizes=[32, 16, 8], dropout_prob=0.2)
+    model.init_weights()
+    output = model.forward(X)
+    print("output_shape:",output.detach().numpy())
+
